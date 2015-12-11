439d5e8572a070ac80b26c06e2f672b7664da990
Allen Kleiner <akleiner24@gmail.com>
2015-12-10 21:53:53 -0600

    Day 5, part 2

--- day_5.py
+++ day_5.py
@@ -31,3 +31,26 @@ for line in f:
     double_letter, bad_string, vowel_count = False, False, 0
 
 print (nice)
+
+# Part 2
+f.seek(0, 0)
+
+def mid_two(string):
+    for i in range(len(string)-1):
+        if string.count(string[i:i+2]) >= 2:
+            return True
+    return False
+
+def no_overlap(string):
+    for i in range(len(string)-2):
+        if string[i] == string[i+2]:
+            return True
+    return False
+
+nice = 0
+
+for line in f:
+    if mid_two(line) and no_overlap(line):
+        nice+=1
+
+print (nice)
